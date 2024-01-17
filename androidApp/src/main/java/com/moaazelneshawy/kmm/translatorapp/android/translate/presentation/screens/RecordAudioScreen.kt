6fba85bafedcd816a0194a3c063fd42082019d1c
MoaazElneshawy <m.elneshawy@gmail.com>
2024-01-17 12:11:10 +0300

    navigate to record audio screen

---
+++ androidApp/src/main/java/com/moaazelneshawy/kmm/translatorapp/android/translate/presentation/screens/RecordAudioScreen.kt
@@ -0,0 +1,11 @@
+package com.moaazelneshawy.kmm.translatorapp.android.translate.presentation.screens
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+const val LANGUAGE_CODE = "languageCode"
+
+@Composable
+fun RecordAudioScreen() {
+    Text(text = "Record Audio screen")
+}
--- androidApp/src/main/java/com/moaazelneshawy/kmm/translatorapp/android/translate/presentation/screens/TranslateScreen.kt
+++ androidApp/src/main/java/com/moaazelneshawy/kmm/translatorapp/android/translate/presentation/screens/TranslateScreen.kt
@@ -7,19 +7,26 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.FabPosition
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalClipboardManager
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.unit.dp
 import com.moaazelneshawy.kmm.translatorapp.android.R
@@ -29,6 +36,7 @@ import com.moaazelneshawy.kmm.translatorapp.android.translate.presentation.compo
 import com.moaazelneshawy.kmm.translatorapp.android.translate.presentation.components.TranslatedHistoryItems
 import com.moaazelneshawy.kmm.translatorapp.android.translate.presentation.components.rememberTextToSpeech
 import com.moaazelneshawy.kmm.translatorapp.android.translate.presentation.padding_10
+import com.moaazelneshawy.kmm.translatorapp.translate.domain.client.TranslateError
 import com.moaazelneshawy.kmm.translatorapp.translate.presentation.TranslateEvent
 import com.moaazelneshawy.kmm.translatorapp.translate.presentation.TranslateState
 import java.util.Locale
@@ -39,10 +47,37 @@ fun TranslateScreen(
     state: TranslateState, onEvent: (TranslateEvent) -> Unit
 ) {
     val context = LocalContext.current
+
+    LaunchedEffect(key1 = state.error, block = {
+        val message = when (state.error) {
+            TranslateError.SERVICE_UNAVAILABLE -> context.getString(R.string.service_unavailble)
+            TranslateError.CLIENT_ERROR -> context.getString(R.string.client_error)
+            TranslateError.SERVER_ERROR -> context.getString(R.string.server_error)
+            TranslateError.UNKNOWN_ERROR -> context.getString(R.string.unkown_error)
+            else -> null
+        }
+        message?.let {
+            Toast.makeText(context, it, Toast.LENGTH_LONG).show()
+            onEvent(TranslateEvent.OnError) // to reset the state after error !
+        }
+    })
     Scaffold(
         floatingActionButton = {
-
-        }
+            FloatingActionButton(
+                onClick = { onEvent(TranslateEvent.RecordAudio) },
+                backgroundColor = MaterialTheme.colors.primary,
+                contentColor = MaterialTheme.colors.onPrimary,
+                modifier = Modifier.size(60.dp)
+            ) {
+                Icon(
+                    imageVector = ImageVector.vectorResource(R.drawable.mic),
+                    contentDescription = stringResource(
+                        id = R.string.record_audio
+                    )
+                )
+            }
+        },
+        floatingActionButtonPosition = FabPosition.Center
     ) { paddingValues ->
 
         LazyColumn(modifier = Modifier
